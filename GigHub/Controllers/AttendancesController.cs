cc2b83fd4a3baba9bc62da7acff69911dc53487d
Debaditya Chatterjee <dchatterjee89@outlook.com>
2019-06-02 20:10:53 -0700

    follow artist

---
+++ GigHub/Controllers/AttendancesController.cs
@@ -0,0 +1,48 @@
+﻿using GigHub.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using GigHub.DTO;
+
+namespace GigHub.Controllers
+{
+    
+
+    public class AttendancesController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public AttendancesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        [Authorize]
+        public IHttpActionResult Attend(AttendanceDto att)
+        {
+            int gigId = att.GigId;
+            var userId = User.Identity.GetUserId();
+
+            if(_context.Attendances.Any(a => a.AttendeeId == userId && a.GigId == gigId))
+            {
+                return BadRequest("Attendance already exists.");
+            }
+
+            var attendance = new Attendance
+            {
+                GigId = gigId,
+                AttendeeId = User.Identity.GetUserId()
+            };
+
+            _context.Attendances.Add(attendance);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
---
+++ GigHub/Controllers/FollowingController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using GigHub.Models;
+using GigHub.DTO;
+using Microsoft.AspNet.Identity;
+
+namespace GigHub.Controllers
+{
+    public class FollowingController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public FollowingController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public IHttpActionResult Follow(FollowingDto dto)
+        {
+            var userId = User.Identity.GetUserId();
+
+            //Check if following already exists
+            if (_context.Followings.Any(f => f.FollowerId == userId && f.FolloweeId == dto.FolloweeId))
+                return BadRequest("You are already following this artist.");
+
+            _context.Followings.Add(new Following {
+                FolloweeId = dto.FolloweeId,
+                FollowerId = userId
+            });
+
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
